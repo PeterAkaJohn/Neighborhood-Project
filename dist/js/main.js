4f84f55edb0bbd900f26270b6f3566c81957fba8
Pierpaolo Iannone <pierpaolo.iannone93@gmail.com>
2017-02-01 00:05:49 +0100

    fixed missing files when running application from within src folder

--- dist/js/main.js
+++ dist/js/main.js
@@ -274,6 +274,9 @@ var Location = function(data) {
     this.favorite = ko.observable(false);
 };
 
+/**
+ * @description Knockout ViewModel
+ */
 var ViewModel = function() {
     var self = this;
 
